b41cceb57fa215af01631ddced0bd940d9e471c6
Valentin Courchelle <valentin@MacBook-Pro-de-Valentin.local>
2023-10-16 16:28:03 +0200

    Project initialization

---
+++ back/src/test/java/alten/demo/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package alten.demo.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
